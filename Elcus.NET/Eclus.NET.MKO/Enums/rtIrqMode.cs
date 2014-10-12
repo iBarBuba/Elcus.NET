e44db04234664bff1220ee6babd9b8b245bde42b
Eugene Alekseev <e.v.alekseev@gmail.com>
2014-10-12 17:09:57 +0400

    RTIrqMode enum

---
+++ Elcus.NET/Eclus.NET.MKO/Enums/rtIrqMode.cs
@@ -0,0 +1,10 @@
+﻿namespace Eclus.NET.MKO.Enums
+{
+    public enum RTIrqMode : ushort
+    {
+        RT_GENER1_BL = (ushort)4,
+        RT_DATA_BL = (ushort)8192,
+        RT_GENER2_BL = (ushort)16384,
+        TMK_IRQ_OFF = (ushort)32768,
+    }
+}
